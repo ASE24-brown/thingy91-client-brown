6675f12555156e411a3d79533297eea74b573c13
YannickKunz <yannick.kuenzli@gmail.com>
2024-11-24 16:11:14 +0100

    feat(Auth) Created sign-up page and put the other pages behind a @login_required

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
+from functools import wraps
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 import requests
+from flask import redirect, url_for, session
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})  # Allow all origins, or specify "http://127.0.0.1:5000" if you want to restrict
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'logged_in' not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
 @app.route('/devices')
+@login_required
 def devices():
     return render_template('devices.html')
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/api/frontend-sensor-data')
 def frontend_sensor_data():
